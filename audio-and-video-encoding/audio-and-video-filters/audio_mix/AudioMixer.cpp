e06d6f5e72d1f776710e053a7cfeab49e11561d7
jwd <jiawdgoo@gmail.com>
2025-12-14 04:55:23 +0800

    feat(audio): 实现音频混音器功能
    - 添加 AudioMixer 类，支持多路音频输入和一路音频输出
    - 实现基于 FFmpeg 的音频过滤器图构建与配置
    - 支持动态添加音频帧和获取混音后音频数据
    - 提供初始化、退出及资源释放接口
    - 更新声道布局 API 适配新版 FFmpeg
    - 添加 CMakeLists 构建配置文件
    - 包含示例程序验证混音功能

---
+++ audio-and-video-encoding/audio-and-video-filters/audio_mix/AudioMixer.cpp
@@ -0,0 +1,325 @@
+#include "audiomixer.h"
+
+AudioMixer::AudioMixer()
+    : initialized_(false)
+    , filter_graph_(nullptr)
+    , audio_output_info_(nullptr)
+{
+
+    audio_mix_info_.reset(new AudioInfo);
+    audio_mix_info_->name = "amix";     // 混音用的
+
+    audio_sink_info_.reset(new AudioInfo);
+    audio_sink_info_->name = "sink";    // 输出
+}
+
+AudioMixer::~AudioMixer()
+{
+    if(initialized_) {
+        exit();
+    }
+}
+
+int AudioMixer::addAudioInput(uint32_t index, uint32_t samplerate, uint32_t channels,
+                              uint32_t bitsPerSample, AVSampleFormat format)
+{
+    std::lock_guard<std::mutex> locker(mutex_);
+
+    if (initialized_)
+    {
+        return -1;
+    }
+
+    if (audio_input_info_.find(index) != audio_input_info_.end())
+    {
+        return -1;      // 已经存在则返回-1
+    }
+
+    // 初始化一个input 可以有多个输入
+    auto& filterInfo = audio_input_info_[index];
+    // 初始化音频相关的参数
+    filterInfo.samplerate = samplerate;
+    filterInfo.channels = channels;
+    filterInfo.bitsPerSample = bitsPerSample;
+    filterInfo.format = format;
+    filterInfo.name = std::string("input") + std::to_string(index);
+
+    return 0;
+}
+
+int AudioMixer::addAudioOutput(const uint32_t samplerate, const uint32_t channels,
+                               const uint32_t bitsPerSample, const AVSampleFormat format)
+{
+    std::lock_guard<std::mutex> locker(mutex_);
+
+    if (initialized_)
+    {
+        return -1;
+    }
+    // 初始化输出相关的参数       只有一个输出
+    audio_output_info_.reset(new AudioInfo);
+    audio_output_info_->samplerate = samplerate;
+    audio_output_info_->channels = channels;
+    audio_output_info_->bitsPerSample = bitsPerSample;
+    audio_output_info_->format = format;
+    audio_output_info_->name = "output";
+    return 0;
+}
+/*
+inputs
+    The number of inputs. If unspecified, it defaults to 2.//输入的数量，如果没有指明，默认为2.
+duration
+    How to determine the end-of-stream.//决定了流的结束
+    longest
+    The duration of the longest input. (default)//最长输入的持续时间
+    shortest
+    The duration of the shortest input.//最短输入的持续时间
+    first
+    The duration of the first input.//第一个输入的持续时间 比如直播，mic为主的时候 first，
+dropout_transition
+    The transition time, in seconds, for volume renormalization when an input stream ends.
+    The default value is 2 seconds.
+*/
+/**
+ * @brief 初始化
+ * @param duration longest最长输入时间,shortest最短,first第一个输入持续的时间
+ * @return
+ */
+int AudioMixer::init(const  char *duration)
+{
+    std::lock_guard<std::mutex> locker(mutex_);
+
+    if (initialized_)
+    {
+        return -1;
+    }
+
+    if (audio_input_info_.size() == 0)
+    {
+        return -1;
+    }
+
+    filter_graph_ = avfilter_graph_alloc(); // 创建avfilter_graph
+    if (filter_graph_ == nullptr)
+    {
+        return -1;
+    }
+
+    char args[512] = {0};
+
+    const AVFilter *amix = avfilter_get_by_name("amix");    // 混音
+    audio_mix_info_->filterCtx = avfilter_graph_alloc_filter(filter_graph_, amix, "amix");
+    /*inputs=输入流数量, duration=决定流的结束,
+     * dropout_transition= 输入流结束时,容量重整时间,
+     * (longest最长输入时间,shortest最短,first第一个输入持续的时间))*/
+    snprintf(args, sizeof(args), "inputs=%d:duration=%s:dropout_transition=0",
+             audio_input_info_.size(), duration);
+    if (avfilter_init_str(audio_mix_info_->filterCtx, args) != 0)
+    {
+        printf("[AudioMixer] avfilter_init_str(amix) failed.\n");
+        return -1;
+    }
+
+    const AVFilter *abuffersink = avfilter_get_by_name("abuffersink");
+    audio_sink_info_->filterCtx = avfilter_graph_alloc_filter(filter_graph_, abuffersink, "sink");
+    if (avfilter_init_str(audio_sink_info_->filterCtx, nullptr) != 0)
+    {
+        printf("[AudioMixer] avfilter_init_str(abuffersink) failed.\n");
+        return -1;
+    }
+
+    for (auto& iter : audio_input_info_)
+    {
+        const AVFilter *abuffer = avfilter_get_by_name("abuffer");
+        // 使用新的声道布局API
+        AVChannelLayout ch_layout;
+        av_channel_layout_default(&ch_layout, iter.second.channels);
+        char layout_str[64];
+        av_channel_layout_describe(&ch_layout, layout_str, sizeof(layout_str));
+        av_channel_layout_uninit(&ch_layout);
+        
+        snprintf(args, sizeof(args),
+                 "sample_rate=%d:sample_fmt=%s:channel_layout=%s",
+                 iter.second.samplerate,
+                 av_get_sample_fmt_name(iter.second.format),
+                 layout_str);
+        printf("[AudioMixer] input(%d) args: %s\n", iter.first, args);
+
+        iter.second.filterCtx = avfilter_graph_alloc_filter(filter_graph_, abuffer,
+                                                            audio_output_info_->name.c_str());
+
+        if (avfilter_init_str(iter.second.filterCtx, args) != 0)
+        {
+            printf("[AudioMixer] avfilter_init_str(abuffer) failed.\n");
+            return -1;
+        }
+        // iter.first 是input index
+        if (avfilter_link(iter.second.filterCtx, 0, audio_mix_info_->filterCtx, iter.first) != 0)
+        {
+            printf("[AudioMixer] avfilter_link(abuffer(%d), amix) failed.", iter.first);
+            return -1;
+        }
+    }
+
+    if (audio_output_info_ != nullptr)
+    {
+        const AVFilter *aformat = avfilter_get_by_name("aformat");
+        // 使用新的声道布局API
+        AVChannelLayout ch_layout;
+        av_channel_layout_default(&ch_layout, audio_output_info_->channels);
+        char layout_str[64];
+        av_channel_layout_describe(&ch_layout, layout_str, sizeof(layout_str));
+        av_channel_layout_uninit(&ch_layout);
+        
+        snprintf(args, sizeof(args),
+                 "sample_rates=%d:sample_fmts=%s:channel_layouts=%s",
+                 audio_output_info_->samplerate,
+                 av_get_sample_fmt_name(audio_output_info_->format),
+                 layout_str);
+        printf("[AudioMixer] output args: %s\n", args);
+        audio_output_info_->filterCtx = avfilter_graph_alloc_filter(filter_graph_, aformat,
+                                                                    "aformat");
+
+        if (avfilter_init_str(audio_output_info_->filterCtx, args) != 0)
+        {
+            printf("[AudioMixer] avfilter_init_str(aformat) failed. %s\n", args);
+            return -1;
+        }
+
+        if (avfilter_link(audio_mix_info_->filterCtx, 0, audio_output_info_->filterCtx, 0) != 0)
+        {
+            printf("[AudioMixer] avfilter_link(amix, aformat) failed.\n");
+            return -1;
+        }
+
+        if (avfilter_link(audio_output_info_->filterCtx, 0, audio_sink_info_->filterCtx, 0) != 0)
+        {
+            printf("[AudioMixer] avfilter_link(aformat, abuffersink) failed.\n");
+            return -1;
+        }
+    }
+
+    if (avfilter_graph_config(filter_graph_, NULL) < 0)
+    {
+        printf("[AudioMixer] avfilter_graph_config() failed.\n");
+        return -1;
+    }
+
+    initialized_ = true;
+    return 0;
+}
+
+int AudioMixer::exit()
+{
+    std::lock_guard<std::mutex> locker(mutex_);
+
+    if (initialized_)
+    {
+        for (auto iter : audio_input_info_)
+        {
+            if (iter.second.filterCtx != nullptr)
+            {
+                avfilter_free(iter.second.filterCtx);
+            }
+        }
+
+        audio_input_info_.clear();
+
+        if (audio_output_info_ && audio_output_info_->filterCtx)
+        {
+            avfilter_free(audio_output_info_->filterCtx);
+            audio_output_info_->filterCtx = nullptr;
+        }
+
+        if (audio_mix_info_->filterCtx)
+        {
+            avfilter_free(audio_mix_info_->filterCtx);
+            audio_mix_info_->filterCtx = nullptr;
+        }
+
+        if (audio_sink_info_->filterCtx)
+        {
+            avfilter_free(audio_sink_info_->filterCtx);
+            audio_sink_info_->filterCtx = nullptr;
+        }
+
+        avfilter_graph_free(&filter_graph_);
+        filter_graph_ = nullptr;
+        initialized_ = false;
+    }
+
+    return 0;
+}
+
+int AudioMixer::addFrame(uint32_t index, uint8_t *inBuf, uint32_t size)
+{
+    std::lock_guard<std::mutex> locker(mutex_);
+
+    if (!initialized_)
+    {
+        return -1;
+    }
+
+    auto iter = audio_input_info_.find(index);
+    if (iter == audio_input_info_.end())
+    {
+        return -1;
+    }
+
+    if(inBuf && size > 0) {
+        std::shared_ptr<AVFrame> avFrame(av_frame_alloc(), [](AVFrame *ptr) { av_frame_free(&ptr); });
+
+        avFrame->sample_rate = iter->second.samplerate;
+        avFrame->format = iter->second.format;
+        // 使用新的声道布局API
+        av_channel_layout_default(&avFrame->ch_layout, iter->second.channels);
+        avFrame->nb_samples = size * 8 / iter->second.bitsPerSample / iter->second.channels;
+
+        av_frame_get_buffer(avFrame.get(), 1);
+        memcpy(avFrame->extended_data[0], inBuf, size);
+
+        if (av_buffersrc_add_frame(iter->second.filterCtx, avFrame.get()) != 0)
+        {
+            return -1;
+        }
+    } else {
+        if (av_buffersrc_add_frame(iter->second.filterCtx, NULL) != 0)
+        {
+            return -1;
+        }
+    }
+
+
+    return 0;
+}
+
+int AudioMixer::getFrame(uint8_t *outBuf, uint32_t maxOutBufSize)
+{
+    std::lock_guard<std::mutex> locker(mutex_);
+
+    if (!initialized_)
+    {
+        return -1;
+    }
+
+    std::shared_ptr<AVFrame> avFrame(av_frame_alloc(), [](AVFrame *ptr) { av_frame_free(&ptr); });
+
+    int ret = av_buffersink_get_frame(audio_sink_info_->filterCtx, avFrame.get());
+
+    if (ret < 0)
+    {
+        //        printf("ret = %d, %d\n", ret, AVERROR(EAGAIN));
+        return -1;
+    }
+
+    // 使用新的声道布局API获取通道数
+    int size = av_samples_get_buffer_size(NULL, avFrame->ch_layout.nb_channels, avFrame->nb_samples, (AVSampleFormat)avFrame->format, 1);
+
+    if (size > (int)maxOutBufSize)
+    {
+        return 0;
+    }
+
+    memcpy(outBuf, avFrame->extended_data[0], size);
+    return size;
+}
